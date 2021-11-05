3ff3a6e705b5a832cd0c10508fb443df1077ac3b
youssef-mohaseb <87242323+youssef-mohaseb@users.noreply.github.com>
2021-11-06 00:35:51 +0200

    Update cw1_code.c

--- UART_ADC/cw1_code.c
+++ UART_ADC/cw1_code.c
@@ -21,7 +21,7 @@ void main() {
     potentiometer = ADC_Read(1);     // ADC COVERT VALUE OF Potentiometer from analouge to degital
 
     if (potentiometer >= 169){      // if Potentiometer value > 169
-        PORTD = 0xff;              // pORT D IS ALL ON
+        PORTD = 0xff;              // PORT D is all on 
         UART1_Write(potentiometer);
         UART1_Write('-');
         UART1_Write_Text("potentiometer greater than 169"); // print on UART Terminal If Potentiometer value > 169 this text ("potentiometer less than 169")
@@ -29,9 +29,9 @@ void main() {
         UART1_write(10);                                    // Space in terminal
 
     }
-    else   // OTHERWISE
+    else                                                         // OTHERWISE
     {
-      PORTD = 0x0f;                                                // PORT D HALF OF & HALF ON
+      PORTD = 0x0f;                                             // PORT D HALF OF & HALF ON
       UART1_Write(potentiometer);
       UART1_Write('-');
       UART1_Write_Text("potentiometer less than 169");        // print on UART Terminal If Potentiometer value < 169 this text ("potentiometer less than 169")
